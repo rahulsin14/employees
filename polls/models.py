aa017dde0061bd677aa43cee6c69ef121cbfdc21
rahul.singla <rahul.singla@innovaccer.com>
2021-08-17 05:02:57 +0530

    updated models

--- polls/models.py
+++ polls/models.py
@@ -3,17 +3,16 @@ from django.db import models
 
 # Create your models here.
 class employees(models.Model):
-    year_month = models.CharField(max_length=100)
-    month_of_release = models.CharField(max_length=100)
-    passenger_type = models.CharField(max_length=100)
-    direction= models.CharField(max_length=100)
-    citizenship= models.CharField(max_length=100)
-    visa= models.CharField(max_length=100)
-    country_of_residence= models.CharField(max_length=100)
-    estimate= models.CharField(max_length=100)
-    standard_error= models.CharField(max_length=100)
-    status= models.CharField(max_length=100)
+    year_month = models.CharField(max_length=2)
+    month_of_release = models.CharField(max_length=2)
+    passenger_type = models.CharField(max_length=2)
+    direction= models.CharField(max_length=2)
+    citizenship= models.CharField(max_length=2)
+    visa= models.CharField(max_length=2)
+    country_of_residence= models.CharField(max_length=2)
+    estimate= models.CharField(max_length=2)
+    standard_error= models.CharField(max_length=2)
+    status= models.CharField(max_length=2)
 
     def __str__(self):
         return self.year_month
-
